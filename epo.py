3c75809624557e11825acc7a661e80967ad782ac
Ontroad <48441131+Ontroad@users.noreply.github.com>
2024-07-22 16:58:33 +0800

    Update epo.py

--- epo.py
+++ epo.py
@@ -31,7 +31,7 @@ class EPO_PMN(PPO):
             self._cfgs.algo_cfgs.update_cycle
             // distributed.world_size()
             // self._cfgs.train_cfgs.vector_env_nums
-        )  
+        )
 
     def _init_model(self) -> None:
         """Initialize the model."""
@@ -82,15 +82,15 @@ class EPO_PMN(PPO):
         self._logger.register_key('Train/target_value_cs')
         self._logger.register_key('Loss/Loss_cost_square_critic', delta=True)
 
-    def learn(self) -> Tuple[Union[int, float], ...]:  
+    def learn(self) -> Tuple[Union[int, float], ...]:
         start_time = time.time()
         self._logger.log('INFO: Start training')
 
-        for epoch in range(self._cfgs.train_cfgs.epochs):  
+        for epoch in range(self._cfgs.train_cfgs.epochs):
             epoch_time = time.time()
 
             roll_out_time = time.time()
-            self._env.roll_out(             
+            self._env.roll_out(
                 steps_per_epoch=self._steps_per_epoch,
                 agent=self._actor_critic,
                 buffer=self._buf,
@@ -100,14 +100,14 @@ class EPO_PMN(PPO):
 
             update_time = time.time()
             self._logger.store(**{'Time/Rollout': time.time() - roll_out_time})
-            self._update()  
+            self._update()
             self._logger.store(**{'Time/Update': time.time() - update_time})
 
             if self._cfgs.model_cfgs.exploration_noise_anneal:
-                self._actor_critic.annealing(epoch)  
+                self._actor_critic.annealing(epoch)
 
             if self._cfgs.model_cfgs.actor.lr != 'None':
-                self._actor_critic.actor_scheduler.step() 
+                self._actor_critic.actor_scheduler.step()
 
             self._logger.store(
                 **{
@@ -115,9 +115,11 @@ class EPO_PMN(PPO):
                     'Time/FPS': self._cfgs.algo_cfgs.update_cycle / (time.time() - epoch_time),
                     'Time/Total': (time.time() - start_time),
                     'Time/Epoch': (time.time() - epoch_time),
-                    'Train/LR': 0.0
-                    if self._cfgs.model_cfgs.actor.lr == 'None'
-                    else self._actor_critic.actor_scheduler.get_last_lr()[0],
+                    'Train/LR': (
+                        0.0
+                        if self._cfgs.model_cfgs.actor.lr == 'None'
+                        else self._actor_critic.actor_scheduler.get_last_lr()[0]
+                    ),
                 }
             )
 
@@ -134,9 +136,9 @@ class EPO_PMN(PPO):
 
         return ep_ret, ep_cost, ep_len
 
-    def _update(self) -> None: 
-        
-        data = self._buf.get()  
+    def _update(self) -> None:
+
+        data = self._buf.get()
         (
             obs,
             act,
@@ -160,13 +162,13 @@ class EPO_PMN(PPO):
             data['adv_r'],
             data['adv_c'],
             data['adv_cs'],
-            data['cost'],  
-            data['discount_cost'],  
+            data['cost'],
+            data['discount_cost'],
             data['time_step'],
         )
 
         original_obs = obs
-        old_distribution = self._actor_critic.actor(obs) 
+        old_distribution = self._actor_critic.actor(obs)
 
         dataloader = DataLoader(
             dataset=TensorDataset(
@@ -185,11 +187,9 @@ class EPO_PMN(PPO):
             ),
             batch_size=self._cfgs.algo_cfgs.batch_size,
             shuffle=True,
-        )  
+        )
 
-        for i in track(
-            range(self._cfgs.algo_cfgs.update_iters), description='Updating...'
-        ): 
+        for i in track(range(self._cfgs.algo_cfgs.update_iters), description='Updating...'):
             for (
                 obs,
                 act,
@@ -200,15 +200,13 @@ class EPO_PMN(PPO):
                 adv_r,
                 adv_c,
                 adv_cs,
-                cost,  
-                discount_cost, 
+                cost,
+                discount_cost,
                 time_step,
             ) in dataloader:
-                self._update_reward_critic(obs, target_value_r) 
+                self._update_reward_critic(obs, target_value_r)
                 if self._cfgs.algo_cfgs.use_cost:
-                    self._update_cost_critic(
-                        obs, target_value_c, target_value_cs
-                    )  
+                    self._update_cost_critic(obs, target_value_c, target_value_cs)
                 self._update_actor(
                     obs,
                     act,
@@ -220,9 +218,9 @@ class EPO_PMN(PPO):
                     discount_cost,
                     target_value_cs,
                     time_step,
-                )  
+                )
 
-            new_distribution = self._actor_critic.actor(original_obs)  
+            new_distribution = self._actor_critic.actor(original_obs)
 
             kl = (
                 torch.distributions.kl.kl_divergence(old_distribution, new_distribution)
@@ -230,7 +228,7 @@ class EPO_PMN(PPO):
                 .mean()
                 .item()
             )
-            kl = distributed.dist_avg(kl) 
+            kl = distributed.dist_avg(kl)
 
             if self._cfgs.algo_cfgs.kl_early_stop and kl > self._cfgs.algo_cfgs.target_kl:
                 self._logger.log(f'Early stopping at iter {i + 1} due to reaching max kl')
@@ -253,10 +251,8 @@ class EPO_PMN(PPO):
         adv_c: torch.Tensor,
         adv_cs: torch.Tensor,
     ) -> None:
-        
-        loss, info = self._loss_safety_pi_via_cost(
-            obs, act, logp, adv_r, adv_c, adv_cs
-        )
+
+        loss, info = self._loss_safety_pi_via_cost(obs, act, logp, adv_r, adv_c, adv_cs)
 
         self._actor_critic.actor_optimizer.zero_grad()
         loss.backward()
@@ -276,36 +272,32 @@ class EPO_PMN(PPO):
         )
 
     def _loss_safety_pi_via_cost(
-            self,
-            obs: torch.Tensor,
-            act: torch.Tensor,
-            logp: torch.Tensor,
-            adv: torch.Tensor,
-            adv_c: torch.Tensor,
-            adv_cs: torch.Tensor,
+        self,
+        obs: torch.Tensor,
+        act: torch.Tensor,
+        logp: torch.Tensor,
+        adv: torch.Tensor,
+        adv_c: torch.Tensor,
+        adv_cs: torch.Tensor,
     ) -> Tuple[torch.Tensor, Dict[str, float]]:
-        distribution = self._actor_critic.actor(obs) 
-        logp_ = self._actor_critic.actor.log_prob(act)  
-        std = self._actor_critic.actor.std  
-        ratio = torch.exp(logp_ - logp)  
+        distribution = self._actor_critic.actor(obs)
+        logp_ = self._actor_critic.actor.log_prob(act)
+        std = self._actor_critic.actor.std
+        ratio = torch.exp(logp_ - logp)
         ratio_cliped = torch.clamp(
             ratio, 1 - self._cfgs.algo_cfgs.clip, 1 + self._cfgs.algo_cfgs.clip
         )
-        surr_adv = torch.min(ratio * adv, ratio_cliped * adv).mean()  
-        pi_obj = (
-                surr_adv + self._cfgs.algo_cfgs.entropy_coef * distribution.entropy().mean()
-        )  
+        surr_adv = torch.min(ratio * adv, ratio_cliped * adv).mean()
+        pi_obj = surr_adv + self._cfgs.algo_cfgs.entropy_coef * distribution.entropy().mean()
         self._logger.store(**{'Train/Reward_obj': pi_obj})
 
-        surr_cadv = (ratio * adv_c).mean()  
-        surr_csadv = (ratio * adv_cs).mean()  
-        JcExp = (
-                self._logger.get_stats('Metrics/EpCost')[0] - self._cfgs.algo_cfgs.cost_limit
-        ) 
-        
+        surr_cadv = (ratio * adv_c).mean()
+        surr_csadv = (ratio * adv_cs).mean()
+        JcExp = self._logger.get_stats('Metrics/EpCost')[0] - self._cfgs.algo_cfgs.cost_limit
+
         penalty_reg_fn = torch.exp(
             torch.clamp(torch.tensor(JcExp), max=self._cfgs.algo_cfgs.penalty_reg_max)
-        )  
+        )
 
         cadv_penalty = self._cfgs.algo_cfgs.alpha * torch.log(
             1.0 + torch.exp(torch.clamp(surr_cadv, max=self._cfgs.algo_cfgs.clip_maximum))
@@ -335,7 +327,7 @@ class EPO_PMN(PPO):
     def _update_cost_critic(
         self, obs: torch.Tensor, target_value_c: torch.Tensor, target_value_cs: torch.Tensor
     ) -> None:
-       
+
         self._actor_critic.cost_critic_optimizer.zero_grad()
         loss_cost = nn.functional.mse_loss(self._actor_critic.cost_critic(obs)[0], target_value_c)
         if self._cfgs.algo_cfgs.use_critic_norm:
@@ -353,16 +345,16 @@ class EPO_PMN(PPO):
         self._logger.store(
             **{'Value/value_c': self._actor_critic.cost_critic(obs)[0].mean().item()}
         )
-      
+
         self._actor_critic.cost_square_critic_optimizer.zero_grad()
         loss_cost_square = nn.functional.mse_loss(
             self._actor_critic.cost_square_critic(obs)[0], target_value_cs
         )
-    
+
         if self._cfgs.algo_cfgs.use_critic_norm:
             for param in self._actor_critic.cost_square_critic.parameters():
                 loss_cost_square += param.pow(2).sum() * self._cfgs.algo_cfgs.critic_norm_coef
-      
+
         loss_cost_square.backward()
         if self._cfgs.algo_cfgs.use_max_grad_norm:
             torch.nn.utils.clip_grad_norm_(
@@ -376,7 +368,7 @@ class EPO_PMN(PPO):
         self._logger.store(
             **{'Value/value_cs': self._actor_critic.cost_square_critic(obs)[0].mean().item()}
         )
-       
+
     def normalization_data(self, data):
         mean = data.mean(dim=0, keepdim=True)
         std = data.std(dim=0, keepdim=True)
